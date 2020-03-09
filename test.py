46e7ba2e8c9404d358a516d26cf6f19e0ab3621a
SarahMinich <minich.sarah@gmail.com>
2020-03-09 10:57:15 +0100

    First try to create a tree from a Prufer sequence

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 import networkx as nx
 
-sequence = [4, 4, 4, 5]
+sequence = [3, 3, 3, 4]
 tree = nx.from_prufer_sequence(sequence)
-print (tree)
+nx.draw_networkx (tree)
+
+
+    # >>> import networkx as nx
+    # >>> G = nx.Graph()
+    # >>> G.add_edge('A', 'B', weight=4)
+    # >>> G.add_edge('B', 'D', weight=2)
+    # >>> G.add_edge('A', 'C', weight=3)
+    # >>> G.add_edge('C', 'D', weight=4)
+    # >>> nx.shortest_path(G, 'A', 'D', weight='weight')
+    # ['A', 'B', 'D']
+    
