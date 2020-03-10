7df076e4984ca985156d502cac9ceefa01c23879
SarahMinich <minich.sarah@gmail.com>
2020-03-10 09:59:04 +0100

    Corrected the test function for the creation of a Tree

--- test_tree.py
+++ test_tree.py
@@ -5,7 +5,8 @@ def test_creation_from_prufer():
     sequence = [3, 3, 3, 4]
     t1 = Tree(sequence)
     t2 = nx.from_prufer_sequence(sequence)
-    assert t1 == t2
+    assert set(t1.tree.nodes) == set(t2.nodes)
+    assert set(t1.tree.edges) == set(t2.edges)
 
 def test_find_leaves():
     sequence = [3, 3, 3, 4]
