1a491673ef564270cbd46f1add83c4e2c31e915b
SarahMinich <minich.sarah@gmail.com>
2021-01-18 17:51:46 +0100

    Update discovery_networkx.py

--- discovery_networkx.py
+++ discovery_networkx.py
@@ -11,13 +11,15 @@ def random_graph_generation_and_dump_in_json():
     d = 0.5
     m = (d * nb_sommets * (nb_sommets - 1)) / 2
     seed = 29
+    graph_type = "normal"
 
     g = nx.gnm_random_graph(nb_sommets, m, seed)
 
     graph_in_data = dict()
     graph_in_data = {
         "seed" : seed,
-        "node_count" : len(g.nodes),
+        "graph_type" : graph_type,
+        "total_node_count" : len(g.nodes),
         "edge_count" : len(list(g.edges)),
         "nodes" : sorted(g.nodes),
         "edges" : list(g.edges)
@@ -27,12 +29,12 @@ def random_graph_generation_and_dump_in_json():
         json_file.write(data_to_json(graph_in_data))
 
 def random_bipartite_graph_generation():
-    nb_sommets = 15
+    nb_sommets = 10
     seed = 29
 
     n1 = random.randint(1, nb_sommets-1)
     n2 = nb_sommets - n1
-    p = 0.5
+    p = 0.85
 
     print("n1 = ", n1)
     print("n2 = ", n2)
@@ -47,6 +49,35 @@ def random_bipartite_graph_generation():
         print("The graph is connected")
     else:
         print("The graph is not connected")
+
+def random_bipartite_graph_generation_and_dump_in_json():
+    nb_sommets = 10
+    seed = 29
+
+    n1 = random.randint(1, nb_sommets-1)
+    n2 = nb_sommets - n1
+    p = 0.85
+
+    print("n1 = ", n1)
+    print("n2 = ", n2)
+
+    g = bipartite.random_graph(n1, n2, p, seed, directed=False)
+    V1, V2 = nx.bipartite.sets(g)
+
+    graph_for_json = dict()
+    graph_for_json = {
+        "seed" : seed,
+        "graph_type" : "bipartite",
+        "total_node_count" : len(g.nodes),
+        "V1_node_count" : len(V1),
+        "V2_node_count" : len(V2),
+        "edge_count" : len(list(g.edges)),
+        "nodes" : sorted(g.nodes),
+        "edges" : list(g.edges)
+    }
+
+    with open('test_dumps/random_bipartite_1.json', 'w', encoding ='utf8') as json_file: 
+        json_file.write(data_to_json(graph_for_json))
 
 def test_connectivity():
     nb_sommets = 15
@@ -72,7 +103,6 @@ def stat_connectivity():
     print("There are ", false_count ,"not connected graphs => ", 100 * false_count/1000, "%.")
 
 def complete_bipartite_graph_generation_and_dump_in_json():
-    print("complete_bipartite_graph_generation_and_dump_in_json")
     nb_sommets = 15
     seed = 29
 
@@ -94,11 +124,11 @@ def complete_bipartite_graph_generation_and_dump_in_json():
         "edges" : list(g.edges)
     }
 
-    with open('test_dumps/bipartite_1.json', 'w', encoding ='utf8') as json_file: 
-        json.dump(graph_for_json, json_file, indent=4)
+    with open('test_dumps/complete_bipartite_1.json', 'w', encoding ='utf8') as json_file: 
+        json.dump(graph_for_json, json_file, indent=4) # Ugly indents
 
-    with open('test_dumps/bipartite_2.json', 'w', encoding ='utf8') as json_file: 
-        json_file.write(data_to_json(graph_for_json))
+    with open('test_dumps/complete_bipartite_2.json', 'w', encoding ='utf8') as json_file: 
+        json_file.write(data_to_json(graph_for_json)) # Nice indents
 
 
 if __name__ == "__main__":
@@ -106,4 +136,5 @@ if __name__ == "__main__":
     #stat_connectivity()
     #test_read_graph_from_json()
     random_graph_generation_and_dump_in_json()
-    random_bipartite_graph_generation()
+    random_bipartite_graph_generation_and_dump_in_json()
+    complete_bipartite_graph_generation_and_dump_in_json()
