b7f7d40d6efdb8b209b305c43bb15a4d79e8d7ed
SarahMinich <minich.sarah@gmail.com>
2021-01-18 19:04:26 +0100

    Created a function to handle random classic graph generation

--- instance_generation.py
+++ instance_generation.py
@@ -79,6 +79,26 @@ def bipartite_generation(tailles, nb_instances, chemin_pour_stockage):
             with open('myfile.json', 'w', encoding ='utf8') as json_file: 
                 json_file.write(data_to_json(graph_for_json))
 
+def classic_generation(tailles, densites, nb_instances, chemin_pour_stockage):
+    graph_type = "classic"
+    for nb_sommets in tailles:
+            for d in densites:
+                m = (d * nb_sommets * (nb_sommets - 1)) / 2
+                for i in range (1, nb_instances):
+                    g = nx.gnm_random_graph(nb_sommets, m, seed=None)
+                    
+                    graph_for_json = dict()
+                    graph_for_json = {
+                        "seed" : seed,
+                        "graph_type" : graph_type,
+                        "total_node_count" : len(g.nodes),
+                        "edge_count" : len(list(g.edges)),
+                        "nodes" : sorted(g.nodes),
+                        "edges" : list(g.edges)
+                    }
+                    name = chemin_pour_stockage + graph_type + "_n=" + str(nb_sommets) + "_d=" + str(d) + "__" + '{:03}'.format(i) + ".json"
+                    with open(name, 'w', encoding ='utf8') as json_file: 
+                        json_file.write(data_to_json(graph_for_json))
 
 if __name__ == "__main__":
     ########################################################################################################
@@ -90,7 +110,7 @@ if __name__ == "__main__":
     nb_instances = 100
     tailles = [25, 50, 75, 100, 200, 500, 1000] #nombre de sommets
     densites = [0.1, 0.2, 0.3, 0.4, 0.5]
-    chemin_pour_stockage = "?"
+    chemin_pour_stockage = "?" # Il faut qu'il termine par un "/" !!!
 
     ########################################################################################################
     ############################################# Generation ###############################################
@@ -109,26 +129,10 @@ if __name__ == "__main__":
                 
     elif (graph_type == "bipartite"):
         bipartite_generation(tailles, nb_instances, chemin_pour_stockage)
-    else:
-        for nb_sommets in tailles:
-            for d in densites:
-                m = (d * nb_sommets * (nb_sommets - 1)) / 2
-                for i in range (1, nb_instances):
-                    if (graph_type == "planar"):
-                        print("CASE: TO DO")
-                    else:
-                        g = nx.gnm_random_graph(nb_sommets, m, seed=None)
-                    
-                    graph_for_json = dict()
-                    graph_for_json = {
-                        "seed" : seed,
-                        "graph_type" : graph_type,
-                        "total_node_count" : len(g.nodes),
-                        "edge_count" : len(list(g.edges)),
-                        "nodes" : sorted(g.nodes),
-                        "edges" : list(g.edges)
-                    }
 
-                    with open('myfile.json', 'w', encoding ='utf8') as json_file: 
-                        json_file.write(data_to_json(graph_for_json))
+    elif (graph_type == "planar"):
+        print("CASE: TO DO")
+
+    else:
+        classic_generation(tailles, densites, nb_instances, chemin_pour_stockage)
 
